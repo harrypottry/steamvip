3913ce08fc48c62f568b3d36f15fbf6b4b68675f
harrypotter12 <zjy_dhls1993@163.com>
2019-11-21 17:02:17 +0800

    源码分析-设计模式-工厂模式

---
+++ src/main/java/com/gupaoedu/vip/pattern/factory/func/YiliFactory.java
@@ -0,0 +1,13 @@
+package com.gupaoedu.vip.pattern.factory.func;
+
+import com.gupaoedu.vip.pattern.factory.Milk;
+import com.gupaoedu.vip.pattern.factory.Yili;
+
+/**
+ * Created by Tom on 2018/3/4.
+ */
+public class YiliFactory implements Factory {
+    public Milk getMilk() {
+        return new Yili();
+    }
+}
