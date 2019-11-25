0247ea0b99e33a4c3d8f2e77f778e4b1af302b55
harrypotter12 <zjy_dhls1993@163.com>
2019-11-25 17:27:41 +0800

    设计模式

---
+++ src/main/java/com/gupaoedu/vip/pattern/observer/mouse/MouseEventType.java
@@ -0,0 +1,19 @@
+package com.gupaoedu.vip.pattern.observer.mouse;
+
+/**
+ * Created by Tom on 2018/3/17.
+ * @author zjy
+ */
+public enum MouseEventType {
+    /**
+     * 点击
+     */
+    ON_CLICK,
+    ON_DOUBLE_CLICK,
+    ON_UP,
+    ON_DOWN,
+    ON_WHEEL,
+    ON_MOVE,
+    ON_OVER
+
+}
